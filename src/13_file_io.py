6e0b78479ac1cae139e1cc92d1bc7528d0f216d6
arturolei <artie.lei@gmail.com>
2020-01-14 22:20:53 -0800

    Finished 13 and got to quote Sir Arthur Conan Doyle's 'His Last Bow'

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,11 +8,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+with open('foo.txt') as foo:
+    print(foo.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open ('bar.txt', 'w') as bar:
+    bar.write("There's an east wind coming all the same, such a wind as never blew on England yet. \n")
+    bar.write("It will be cold and bitter, Watson, and a good many of us may wither before its blast. \n")
+    bar.write("But it's God's own wind none the less and a cleaner, better stronger land will lie in the sunshine when the storm has cleared.")
+
+with open('bar.txt') as bar:
+    print(bar.read())
